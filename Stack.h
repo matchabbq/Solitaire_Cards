8fd1b468f534697bf74108673aff5ee395876aca
KevinCJHuang <47678159+KevinCJHuang@users.noreply.github.com>
2019-06-13 20:21:38 -0400

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -1,12 +1,10 @@
 // Header file for Stack ADT
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <string>
 #include <assert.h>
 #include <stdbool.h>
 #include <iostream>
 
-//#ifndef, #define and #endif prevents redefinition of structures
 #ifndef card_stack_struct
 #define card_stack_struct
 struct card {
@@ -27,7 +25,7 @@ void stack_destroy(struct stack* stk);
 
 void stack_push(struct stack* stk, struct card c);
 
-struct card stack_top(const struct stack* stk);
+card stack_top(const struct stack* stk);
 
 struct card stack_pop(struct stack* stk);
 
